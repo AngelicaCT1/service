743ca5627bd01acb12a58b44325247dfac9458a5
Rapiwash <sistemarapiwash@gmail.com>
2024-05-24 15:10:37 -0500

    new conn a base de datos angelica

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      'mongodb+srv://LaundrySystem:S6GBsMz4smg59lUw@sistema-lavanderia.6e5dgsu.mongodb.net/db-laundry-system?retryWrites=true&w=majority',
+      // 'mongodb+srv://LaundrySystem:S6GBsMz4smg59lUw@sistema-lavanderia.6e5dgsu.mongodb.net/db-laundry-system?retryWrites=true&w=majority',
+      "mongodb+srv://eskala1105:XDSnYD9eVarYWjFK@cluster0.ljcnjjt.mongodb.net/db-angelica?retryWrites=true&w=majority&appName=Cluster0",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       }
     );
-    console.log('Conexión exitosa a MongoDB');
+    console.log("Conexión exitosa a MongoDB");
   } catch (error) {
-    console.error('Error al conectar a MongoDB:', error);
+    console.error("Error al conectar a MongoDB:", error);
     process.exit(1); // Terminar el proceso con error
   }
 };
