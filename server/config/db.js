68da85adaa31d8e0cbd0cef74577527d5b7e8c1e
Rapiwash <sistemarapiwash@gmail.com>
2024-06-02 21:29:37 -0500

    mejora de consulta

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      // 'mongodb+srv://LaundrySystem:S6GBsMz4smg59lUw@sistema-lavanderia.6e5dgsu.mongodb.net/db-laundry-system?retryWrites=true&w=majority',
       "mongodb+srv://eskala1105:XDSnYD9eVarYWjFK@cluster0.ljcnjjt.mongodb.net/db-angelica?retryWrites=true&w=majority&appName=Cluster0",
       {
         useNewUrlParser: true,
